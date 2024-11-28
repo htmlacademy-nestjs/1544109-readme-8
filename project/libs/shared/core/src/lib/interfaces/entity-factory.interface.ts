1db0752130bca58497aee3a5c6ddd7e8f9f5497e
Nina Lukashenko <nina.lucashenco@gmail.com>
2024-11-28 15:34:53 +0100

    Add interfaces: Storable Entity, Entity Factory

---
+++ project/libs/shared/core/src/lib/interfaces/entity-factory.interface.ts
@@ -0,0 +1,12 @@
+import { StorableEntity } from './storable-entity.interface';
+
+// NOTE: factory will create instances of an entity from their plain object representation
+// NOTE: E(ntity) - has more properties that P(ojo)
+
+export interface EntityFactory<E extends StorableEntity<ReturnType<E['toPOJO']>>> {
+  create(POJO: ReturnType<E['toPOJO']>): E;
+}
+
+// EntityFactory is about recreating the entity from that plain object
+
+// EntityFactory<E>: A factory that creates entities from their plain object representations
