89b74cc80925584aa0d1b57c1b3b194bb2c0f95e
Nina Lukashenko <nina.lucashenco@gmail.com>
2024-12-09 11:41:42 +0100

    Add library: Account Config

--- project/libs/shared/helpers/eslint.config.js
+++ project/libs/shared/helpers/eslint.config.js
@@ -1,4 +1,3 @@
-const baseConfig = require('../../../eslint.base.config.js');
 const baseConfig = require('../../../eslint.config.js');
 
-module.exports = [...baseConfig, ...baseConfig];
+module.exports = [...baseConfig];
