ea00a01541c63f9d7745c48e797730e3e9666c72
Nina Lukashenko <nina.lucashenco@gmail.com>
2024-12-17 15:10:17 +0100

    Add health check for account db

--- project/libs/account/configuration/src/lib/mongodb/mongo.config.ts
+++ project/libs/account/configuration/src/lib/mongodb/mongo.config.ts
@@ -14,12 +14,12 @@ export interface MongoConfig {
 
 const getDBConfig = async (): Promise<MongoConfiguration> => {
   const config = plainToClass(MongoConfiguration, {
-    name: process.env['README_MONGO_DB'],
-    host: process.env['README_MONGO_HOST'],
-    port: process.env['README_MONGO_PORT'] ? parseInt(process.env['README_MONGO_PORT'], 10) : DEFAULT_MONGO_PORT,
-    user: process.env['README_MONGO_USER'],
-    password: process.env['README_MONGO_PASSWORD'],
-    authBase: process.env['README_MONGO_AUTH_BASE'],
+    name: process.env['ACCOUNT_MONGO_DB'],
+    host: process.env['ACCOUNT_MONGO_HOST'],
+    port: process.env['ACCOUNT_MONGO_PORT'] ? parseInt(process.env['ACCOUNT_MONGO_PORT'], 10) : DEFAULT_MONGO_PORT,
+    user: process.env['ACCOUNT_MONGO_USER'],
+    password: process.env['ACCOUNT_MONGO_PASSWORD'],
+    authBase: process.env['ACCOUNT_MONGO_AUTH_BASE'],
   });
 
   await config.validate();
